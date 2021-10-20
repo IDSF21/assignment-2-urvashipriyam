743597eb50eb1917d7f2e9642aa4885e35b0f049
urvashipriyam <upkumar@andrew.cmu.edu>
2021-10-19 20:35:33 -0400

    Add descriptive messages to all the graphs

--- main.py
+++ main.py
@@ -59,7 +59,17 @@ with open('data/custom.geo.json') as f:
 
 page1 = True if (sport_selectbox == "Aggregated Olympic Data") else False
 
+st.write("<h1>Olympic Games Over The Years</h1>",
+         unsafe_allow_html=True)
+
 if (page1 == False):
+    st.write("""<p>This is a visualization of the number of medals won by 
+        participant countries in every Olympic game. 
+        Please use the slider to change the Olympic game year.
+        You can apply filters to see the data for the selected sports. 
+        Use the dropdown to select the sports for which you want to filter the data.
+        Use "All" to see the data for all the sports.</p>""",
+             unsafe_allow_html=True)
     year_slider_for_df_with_sports = st.slider(
         'Year (WorldMap With Sports)', min(df_with_sports['Year'].astype(int)), max(df_with_sports['Year'].astype(int)), step=4)
     sport_option = st.multiselect('Select Sport',
@@ -89,11 +99,18 @@ if (page1 == False):
             st.plotly_chart(fig, use_container_width=True)
     else:
         if (page1 == False):
-            st.write("<h2>No Data Available!</h2><br/><br/><br/><br/>",
+            st.write("<h3>No Data Available!</h3>",
                      unsafe_allow_html=True)
 
 else:
     # Add a slider to the sidebar:
+    st.write("""<p>This is a visualization of the total number of medals won by participant countries in every Olympic game. 
+        This chart can be used in conjunction with the bubble chart below in order to better highlight 
+        which countries have been dominating the Olympics. 
+        The medal count is scaled where Gold = 3 medals, Silver = 2 medals and Bronze = 1 medal. 
+        For an exact count of medals please check the chart on page 2. 
+        This is an interactive map, please feel free to zoom, pan or box select any specific areas.</p>""",
+             unsafe_allow_html=True)
     year_slider_for_worldmap = st.slider('Year (WorldMap)', min(df['Year']), max(
         df['Year']), step=4)
 
@@ -109,6 +126,9 @@ else:
 
     st.plotly_chart(fig, use_container_width=True)
 
+    st.write("""<hr/><p>This is a visualization of the medal count of each participant country on a graph of Population vs Per-Capita GDP. 
+        This is an interactive plot. Please feel free to zoom, pan or select different years through the slider.</p>""",
+             unsafe_allow_html=True)
     year_slider_for_bubble = st.slider('Year (Graph)', min(df['Year']), max(
         df['Year']), step=4)
     df_year_bubble = df.loc[df['Year'] == year_slider_for_bubble, :]
